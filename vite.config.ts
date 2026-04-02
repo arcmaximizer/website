13f061163f35cde62bbd4a83c4fb04c51d706bd1
arc <arcmaximizer@proton.me>
2026-04-02 18:50:24 +0000

    fix: spa behavior

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,22 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { readdirSync } from "node:fs";
+
+const htmlFiles = readdirSync(".")
+  .filter((f) => f.endsWith(".html"))
+  .reduce(
+    (acc, f) => {
+      acc[f.replace(".html", "")] = f;
+      return acc;
+    },
+    {} as Record<string, string>,
+  );
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: htmlFiles,
+    },
+  },
 });
